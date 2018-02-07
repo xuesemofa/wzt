2b565f6fc13e16c4e2126148f7638363432b5ce8
xuesemofa@126.com <xuesemofa@126.com>
2018-02-07 16:05:24 +0800

    调整调用接口地址

--- src/main/java/org/client/com/api/AccountInterface.java
+++ src/main/java/org/client/com/api/AccountInterface.java
@@ -9,7 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "http://localhost:9002", name = "account", fallback = AccountFallBack.class)
+@FeignClient(value = "http://39.106.33.113:9002", name = "account", fallback = AccountFallBack.class)
 public interface AccountInterface {
     @RequestMapping(value = "/account/account/acc", method = RequestMethod.GET)
     ResponseResult<AccountModel> getAccount(@Param(value = "account") String account);
@@ -17,7 +17,7 @@ public interface AccountInterface {
     @RequestMapping(value = "/account/account/id", method = RequestMethod.GET)
     ResponseResult getLanders(@Param(value = "id") String id);
 
-    @RequestMapping(value = "/account/account",
+    @RequestMapping(value = "/account/account/account",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseResult register(@Param("model") AccountModel model);
